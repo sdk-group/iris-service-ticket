3887d115f622e2bb7f1893e5351d56b56da80e14
YuuKohaku <heimaolee@gmail.com>
2016-01-24 00:59:11 +0600

    Op control=panel routes 75%

--- src/Ticket/ticket.js
+++ src/Ticket/ticket.js
@@ -14,22 +14,75 @@ class Ticket {
 	}
 
 	//API
-	changeState(ticket, state, reason) {
-		return this.getById(ticket)
-			.then((ticket_data) => {
-				let tick = ticket_data[0];
-				tick.state = state;
-				//@TODO send history entry here
-				return this.iris.setTicket(tick);
+	actionCallAgain({
+		user_id, ticket
+	}) {
+		let data = {
+
+		};
+		return this.iris.getTicket({
+				keys: ticket
+			})
+			.then((tick) => {
+				this.emitter.emit('broadcast', {
+					module: 'ticket',
+					event: 'call',
+					data: {
+						ticket: tick[ticket]
+					}
+				});
+				return this.emitter.addTask('history', {
+					_action: 'set-entries',
+					data
+				});
 			});
 	}
 
-	removeTicket(ticket, reason) {}
 
-	getByPIN(pin) {
+	actionChangeState({
+		ticket, state, reason = 'system'
+	}) {
+		let allowed_transform = {
+			"registered=>called": true,
+			"booked=>registered": true,
+			"called=>postponed": true,
+			"postponed=>registered": true,
+			"called=>processing": true,
+			"processing=>closed": true
+		};
+		return this.iris.getTicket({
+				keys: ticket
+			})
+			.then((tick) => {
+				let data = tick[ticket];
+				let old_state = data.state;
+				if(!(state === old_state) && !allowed_transform[_.join([old_state, state], "=>")])
+					return Promise.reject(new Error("State change not allowed."));
+				data.state = state;
+				return this.iris.setTicket(data);
+			})
+			.then((res) => {
+				return {
+					success: !!(res[ticket] && res[ticket].cas)
+				};
+			})
+			.catch((err) => {
+				return {
+					success: false
+				};
+			});
+	}
+
+	removeTicket({
+		ticket, reason
+	}) {}
+
+	actionByCode({
+		code
+	}) {
 		return this.iris.getTicket({
 				query: {
-					code: pin.toString()
+					code
 				}
 			})
 			.then((res) => {
@@ -37,24 +90,52 @@ class Ticket {
 			});
 	}
 
-	getById(id) {
+	actionById({
+		ticket
+	}) {
 		return this.iris.getTicket({
-				keys: id.toString()
+				keys: ticket
 			})
 			.then((res) => {
 				return _.values(res);
 			});
 	}
 
-	getHistory(ticket) {}
+	getHistory({
+		ticket
+	}) {
+		return this.emitter.addTask('history', {
+			_action: 'get-entries',
+			object: ticket
+		});
+	}
 
-	changePriority(ticket, priority_level, reason) {
-		return this.getById(ticket)
-			.then((ticket_data) => {
-				let tick = ticket_data[0];
+	actionSetPriority({
+		ticket, priority, reason
+	}) {
+		let data = {
+
+		};
+		return this.iris.getTicket({
+				keys: ticket
+			})
+			.then((res) => {
+				let tick = res[ticket];
 				tick.priority = priority_level;
-				//@TODO send history entry here
 				return this.iris.setTicket(tick);
+			})
+			.then((res) => {
+				if(!res[ticket].cas)
+					return Promise.reject(new Error("Failed to set ticket priority."));
+				return this.emitter.addTask('history', {
+					_action: 'set-entries',
+					data
+				});
+			})
+			.catch((err) => {
+				return {
+					success: false;
+				}
 			});
 	}
 
